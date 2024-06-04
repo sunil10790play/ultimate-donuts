ead1164d52cde317f7f456bd01a1d67d562297d1
Sunil Kumar C <sunil10790play@gmail.com>
2024-06-04 08:43:39 +0530

    Add hardcoded donuts data

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -3,13 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-donut-list',
   template: `
-    <p>
-      donut-list works!
-    </p>
+    <div>
+      <div>{{ this.donuts[0].name }}</div>
+    </div>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class DonutListComponent {
+  donuts!: any[];
 
+  ngOnInit() {
+    this.donuts = [
+      {
+        id: 'y8z0As',
+        name: 'Just Chocolate',
+        icon: 'just-chocolate',
+        price: 119,
+        description: 'For the pure chocoholic.',
+      },
+      {
+        id: '3u98Kl',
+        name: 'Glazed Fudge',
+        icon: 'glazed-fudge',
+        price: 129,
+        description: 'Sticky perfection.',
+      },
+      {
+        id: 'ae098s',
+        name: 'Caramel Swirl',
+        icon: 'caramel-swirl',
+        price: 129,
+        description: 'Chocolate drizzled with caramel.',
+      },
+    ];
+  }
 }
